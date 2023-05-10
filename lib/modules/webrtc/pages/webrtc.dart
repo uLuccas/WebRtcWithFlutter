e91769aae01fa5dfebcaf49f8421d9faee75bc70
Luccas <luccassanches29@hotmail.com>
2023-05-10 15:11:12 -0300

    funcionando mais ou menos

--- lib/modules/webrtc/pages/webrtc.dart
+++ lib/modules/webrtc/pages/webrtc.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class WebRtcPage extends StatefulWidget {
   const WebRtcPage({super.key});
@@ -10,56 +13,147 @@ class WebRtcPage extends StatefulWidget {
 
 class _WebRtcPageState extends State<WebRtcPage> {
   // Declara dois obj, um para o usuario remoto e outra para o usuário local
-  final RTCVideoRenderer _localRenderer = RTCVideoRenderer();
-  final RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
+  late final IO.Socket socket;
+  final _localRenderer = RTCVideoRenderer();
+  final _remoteRenderer = RTCVideoRenderer();
+  MediaStream? _localStream;
+  RTCPeerConnection? pc;
 
   @override
   void initState() {
-    _localRenderer.initialize();
-    _remoteRenderer.initialize();
+    init();
     super.initState();
   }
 
-  @override
-  void dispose() {
-    _localRenderer.dispose();
-    _remoteRenderer.dispose();
-    super.dispose();
+  Future init() async {
+    await _localRenderer.initialize();
+    await _remoteRenderer.initialize();
+
+    await connectSocket();
+    await joinRoom();
   }
 
-  static const Map<String, dynamic> _configuration = {
-    'iceServers': [
-      {
-        'urls': [
-          'stun:stun1.l.google.com:19302',
-          'stun:stun2.l.google.com:19302',
-        ],
-      },
-    ],
-  };
+  Future connectSocket() async {
+    socket = IO.io('http://localhost:3000',
+        IO.OptionBuilder().setTransports(['websocket']).build());
+    socket.onConnect((data) => print('연결 완료 !'));
 
-  // Habilitando cam
-  Future<void> enableUserMediaStream() async {
-    var stream = await navigator.mediaDevices.getUserMedia(
-      {
-        'video': true,
-        //  'audio': true
-      },
-    );
-    // emit(state.copyWith(localStream: stream));
-    print("========= STREAM ========");
-    print(stream);
+    socket.on('joined', (data) {
+      _sendOffer();
+    });
+
+    socket.on('offer', (data) async {
+      data = jsonDecode(data);
+      await _gotOffer(RTCSessionDescription(data['sdp'], data['type']));
+      await _sendAnswer();
+    });
+
+    socket.on('answer', (data) {
+      data = jsonDecode(data);
+      _gotAnswer(RTCSessionDescription(data['sdp'], data['type']));
+    });
+
+    socket.on('ice', (data) {
+      data = jsonDecode(data);
+      _gotIce(RTCIceCandidate(
+          data['candidate'], data['sdpMid'], data['sdpMLineIndex']));
+    });
   }
 
-  Future<void> _createPeerConnection() async {
-    final peerConnection = await createPeerConnection(_configuration);
-    // emit(state.copyWith(peerConnection: peerConnection));
-    print("========= PEER CONNECTION ========");
-    print(peerConnection);
+  Future joinRoom() async {
+    final config = {
+      'iceServers': [
+        {"url": "stun:stun.l.google.com:19302"},
+      ]
+    };
+
+    final sdpConstraints = {
+      'mandatory': {
+        'OfferToReceiveAudio': true,
+        'OfferToReceiveVideo': true,
+      },
+      'optional': []
+    };
+
+    pc = await createPeerConnection(config, sdpConstraints);
+
+    final mediaConstraints = {
+      'audio': false,
+      'video': {'facingMode': 'user'}
+    };
+
+    _localStream = await Helper.openCamera(mediaConstraints);
+
+    _localStream!.getTracks().forEach((track) {
+      pc!.addTrack(track, _localStream!);
+    });
+
+    _localRenderer.srcObject = _localStream;
+
+    pc!.onIceCandidate = (ice) {
+      _sendIce(ice);
+    };
+
+    pc!.onAddStream = (stream) {
+      _remoteRenderer.srcObject = stream;
+    };
+
+    socket.emit('join');
+  }
+
+  Future _sendOffer() async {
+    print('send offer');
+    var offer = await pc!.createOffer();
+    pc!.setLocalDescription(offer);
+    socket.emit('offer', jsonEncode(offer.toMap()));
+  }
+
+  Future _gotOffer(RTCSessionDescription offer) async {
+    print('got offer');
+    pc!.setRemoteDescription(offer);
+  }
+
+  Future _sendAnswer() async {
+    print('send answer');
+    var answer = await pc!.createAnswer();
+    pc!.setLocalDescription(answer);
+    socket.emit('answer', jsonEncode(answer.toMap()));
+  }
+
+  Future _gotAnswer(RTCSessionDescription answer) async {
+    print('got answer');
+    pc!.setRemoteDescription(answer);
+  }
+
+  Future _sendIce(RTCIceCandidate ice) async {
+    socket.emit('ice', jsonEncode(ice.toMap()));
+  }
+
+  Future _gotIce(RTCIceCandidate ice) async {
+    pc!.addCandidate(ice);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Row(
+        children: [
+          Expanded(
+              child: Column(
+            children: [
+              Text("Local"),
+              RTCVideoView(_localRenderer),
+            ],
+          )),
+          Expanded(
+              child: Column(
+            children: [
+              Text("Remote"),
+              RTCVideoView(_remoteRenderer),
+            ],
+          )),
+        ],
+      ),
+    );
   }
 }
